b65b3cfe9a293294f35dc093822cabee5e5c8c2e
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2020-12-18 09:31:03 -0600

    Add login route

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // Models
 // const User = require('../models/User');
@@ -43,6 +44,45 @@ router.post('/register', (req, res) => {
                     .catch(error => console.log(error));
                  })
              })
+        }
+    })
+})
+
+// POST controllers/users/login (Public)
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Find a user via the email
+    db.User.findOne({ email })
+    .then(user => {
+        // If no user 
+        if (!user) {
+            res.status(400).json({ msg: 'User not found' })
+        } else {
+            // A user is found
+            bcrypt.compare(password, user.password)
+            .then(isMatch => {
+                // Check password for match
+                if (isMatch) {
+                    // User match, send a JSON web token
+                    // Create a token payload
+                    const payload = {
+                        id: user.id,
+                        email: user.email,
+                        name: user.name
+                    };
+                    // Sign token
+                    jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' }, (error, token) => {
+                        res.json({
+                            success: true,
+                            token: `Bearer ${token}`
+                        });
+                    });
+                } else {
+                    return res.status(400).json({ msg: 'Email or password is incorrect' });
+                }
+            })
         }
     })
 })
